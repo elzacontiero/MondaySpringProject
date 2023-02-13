a2ccfeecbe7f3f876bdff9aae9ca271f12eec9d3
Elza Contiero <contieroelza@gmail.com>
2023-02-13 11:45:23 +0000

    Create the initial Spring Web App

---
+++ src/test/java/com/spartaglobal/mondayspringproject/MondayspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.mondayspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondayspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
