e9f459ed89fdb9070758b8d1da5bbe52bdb825f1
Elza Contiero <contieroelza@gmail.com>
2023-02-13 16:16:05 +0000

    Adding ActorDTO

---
+++ src/main/java/com/spartaglobal/mondayspringproject/dto/ActorDTO.java
@@ -0,0 +1,42 @@
+package com.spartaglobal.mondayspringproject.dto;
+
+import java.time.Instant;
+
+public class ActorDTO {
+    private String firstName;
+    private String lastName;
+    private Instant lastUpdate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "ActorDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", lastUpdate=" + lastUpdate +
+                '}';
+    }
+}
