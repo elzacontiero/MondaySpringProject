6f3745eeff0c0aa09cad7248b915c8f06ffeeea8
Jade Sale <jadesale1991@googlemail.com>
2023-02-13 17:32:39 +0000

    Added unit tests for film tests

--- src/test/java/com/spartaglobal/mondayspringproject/FilmTests.java
+++ src/test/java/com/spartaglobal/mondayspringproject/FilmTests.java
@@ -1,4 +1,35 @@
 package com.spartaglobal.mondayspringproject;
 
+import com.spartaglobal.mondayspringproject.dao.ActorDAO;
+import com.spartaglobal.mondayspringproject.repositories.ActorRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class FilmTests {
+    @Autowired
+    private ActorRepository repo;
+
+    @Test
+    void findFilmByActor(){
+       var actor = new ActorDAO(repo).getActorDTOByName("ed", "chase");
+       // film dao to parse actor.getId()
+        System.out.println(actor);
+        assertEquals("ed chase", actor);
+    }
+
+
+    // testing to see if this runs, as I am getting an error
+    @Test
+    void test(){
+        assertTrue(true);
+    }
+
+
+
+
 }
