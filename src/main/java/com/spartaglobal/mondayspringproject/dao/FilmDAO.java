19683999027dd66da54822b14e88dd6f7a839380
Jade Sale <jadesale1991@googlemail.com>
2023-02-15 11:57:44 +0000

    Current raw working version of find films by actor name feature.

---
+++ src/main/java/com/spartaglobal/mondayspringproject/dao/FilmDAO.java
@@ -0,0 +1,27 @@
+package com.spartaglobal.mondayspringproject.dao;
+
+import com.spartaglobal.mondayspringproject.entities.Film;
+import com.spartaglobal.mondayspringproject.entities.FilmActor;
+import com.spartaglobal.mondayspringproject.repositories.FilmRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FilmDAO {
+    private FilmRepository repo;
+
+    public FilmDAO(FilmRepository repo){
+        this.repo = repo;
+    }
+
+    public List<String > getFilmTitle(List<FilmActor> filmActors){ //1 2 3 4 5
+        List<String> listTitleFilm = new ArrayList<>();
+        for(FilmActor f : filmActors){
+            String title = f.getFilm().getTitle();
+
+            listTitleFilm.add(title);
+        }
+        return listTitleFilm;
+    }
+}
--- src/test/java/com/spartaglobal/mondayspringproject/ActorTests.java
+++ src/test/java/com/spartaglobal/mondayspringproject/ActorTests.java
@@ -1,35 +1,58 @@
 package com.spartaglobal.mondayspringproject;
 
 import com.spartaglobal.mondayspringproject.dao.ActorDAO;
+import com.spartaglobal.mondayspringproject.dao.FilmActorDAO;
+import com.spartaglobal.mondayspringproject.dao.FilmDAO;
+import com.spartaglobal.mondayspringproject.entities.Film;
 import com.spartaglobal.mondayspringproject.repositories.ActorRepository;
-import jakarta.transaction.Transactional;
+import com.spartaglobal.mondayspringproject.repositories.FilmActorRepository;
+import com.spartaglobal.mondayspringproject.repositories.FilmRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class FilmTests {
+@Transactional
+public class ActorTests {
     @Autowired
     private ActorRepository repo;
+    @Autowired
+    private FilmActorRepository faRepo;
+    @Autowired
+    private FilmRepository fRepo;
 
-//    @Test
-//    void findFilmByActor(){
-//       var actor = new ActorDAO(repo).getActorDTOByName("ed", "chase");
-//       // film dao to parse actor.getId()
-//        System.out.println(actor);
-//        assertEquals("ed chase", actor);
-//    }
-
-
-    // testing to see if this runs, as I am getting an error
     @Test
-    void test(){
-        assertEquals("true","true");
+    void findActorId(){
+       var actor = new ActorDAO(repo).getActorDTOByName("ed", "chase");
+       var id = actor.getId();
+        System.out.println(id);
+        assertEquals(3, id);
     }
 
+    @Test
+    void findFilmId(){
+        var filmId = faRepo.findAllByActor_Id(3);
+        System.out.println(filmId);
+    }
+
+
+
+    @Test
+    void getFilmTitleByActorName(){
+        var actor = new ActorDAO(repo).getActorDTOByName("ed", "chase");
+        var id = actor.getId();
+        var filmActorList = faRepo.findAllByActor_Id(id); // returns all films id by actor id
+        var films = new FilmDAO(fRepo).getFilmTitle(filmActorList); // returns all films by film id
+        System.out.println(films); // should show film titles
+        assertTrue(films.size() == 22);
+    }
 
 
 
